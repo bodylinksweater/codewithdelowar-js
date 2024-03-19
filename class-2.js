9238638328597bf526a93969f42fc445a58a2358
bodylinksweater <bodylinksweater@gmail.com>
2024-03-19 15:55:58 +0600

    first commit

--- class-2.js
+++ class-2.js
@@ -1,4 +1,17 @@
 var name = "Mehedi"
 
 
-console.log(name);
+// console.log(name);
+
+
+var name1 = "Mehedi";
+var name2 = "Hasan";
+
+// console.log(name1);
+
+
+
+var firstname = "Mehedi";
+var lastname = "Hasan";
+
+console.log(firstname);
